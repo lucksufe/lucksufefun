b60511f408b968ecc22431233757f1c8ea4c0eb6
fsun <sun.fei@sdmicros.com>
2024-03-27 17:56:22 +0800

    first commit

---
+++ components/IndexApp.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="index-app" v-show="appData.is_show" :title="appData.description">        
+        <a :href="appData.url" target="_blank">
+            <img :src="appData.icon" height="64" width="64"/>
+        </a>
+        {{appData.title}}
+    </div>
+</template>
+
+<!-- 动态加载了appData的数据，没有的话上面的appData.title会报错，因为没有appData -->
+<script setup lang="ts"> 
+const props = defineProps<{
+	appData: any,
+	idx: number
+}>()
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>	
+	<div ref="container">
+		<div id="nav-container">			
+			<IndexApp v-for="(app, i) in app_list" :idx="i" :appData="app"></IndexApp>
+		</div>
+	</div>
+</template>
+<script setup lang="ts">
+import app_list from '../config/applications.json'
+</script>
+<style></style>
