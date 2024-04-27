c49451c895dc7f1aa098e209fc8238f6acf1b429
sunfei <lucksufe@163.com>
2024-04-27 21:28:33 +0800

    1

--- app.vue
+++ app.vue
@@ -3,3 +3,23 @@
     <NuxtPage />
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: '图标导航',
+  meta: [
+    { name: 'description', content: '图标导航' }
+  ],
+  bodyAttrs: {
+    class: '导航'
+  },
+  script: [ { innerHTML: 'console.log(\'百亿补贴\')' } ]
+})
+useSeoMeta({
+  title: '图标导航',
+  ogTitle: '图标导航',
+  description: '简单的图标导航',
+  ogDescription: '磁贴样式的APP图标',
+  ogImage: 'https://pdd.lucksufe.fun/favicon.ico',
+})
+</script>
