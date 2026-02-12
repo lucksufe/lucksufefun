3eb60c9e12d3ffd82de81d0fb443a5789a3ae778
lucksufe <459397707@qq.com>
2026-02-12 14:11:27 +0800

    Change page title from '蛙蛙工具箱' to '工具箱'

--- app.vue
+++ app.vue
@@ -6,7 +6,7 @@
 
 <script setup lang="ts">
 useHead({
-  title: '蛙蛙工具箱',
+  title: '工具箱',
   meta: [
     { name: 'description', content: '工具箱' }
   ],
@@ -16,7 +16,7 @@ useHead({
   script: [ { innerHTML: 'console.log(\'百亿补贴\')' } ]
 })
 useSeoMeta({
-  title: '蛙蛙工具箱',
+  title: '工具箱',
   ogTitle: '图标导航',
   description: '简单的图标导航',
   ogDescription: '磁贴样式的APP图标',
